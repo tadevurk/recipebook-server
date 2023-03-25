4390577324758b3400063474640fd89c9ca4d7dc
Vedat Türk <683343@student.inholland.nl>
2023-03-25 16:56:45 +0100

    user crud

--- app/controllers/usercontroller.php
+++ app/controllers/usercontroller.php
@@ -16,60 +16,56 @@ class UserController extends Controller
         $this->service = new UserService();
     }
 
-    public function login() {
+    // get all users
+    function getAll()
+    {
+        $offset = NULL;
+        $limit = NULL;
 
-        // read user data from request body
-        $postedUser = $this->createObjectFromPostedJson("Models\\User");
+        if (isset($_GET["offset"]) && is_numeric($_GET["offset"])) {
+            $offset = $_GET["offset"];
+        }
+        if (isset($_GET["limit"]) && is_numeric($_GET["limit"])) {
+            $limit = $_GET["limit"];
+        }
+        $users = $this->service->getAll($offset, $limit);
+        $this->respond($users);
+    }
 
-        // get user from db
-        $user = $this->service->checkUsernamePassword($postedUser->username, $postedUser->password);
+    // get one user
+    function getOne($id)
+    {
+        $user = $this->service->getOne($id);
 
-        // if the method returned false, the username and/or password were incorrect
-        if(!$user) {
-            $this->respondWithError(401, "Invalid login");
+        // we might need some kind of error checking that returns a 404 if the user is not found in the DB
+        if (!$user) {
+            $this->respondWithError(404, "User not found");
             return;
         }
 
-        // generate jwt
-        $tokenResponse = $this->generateJwt($user);       
-
-        $this->respond($tokenResponse);    
+        $this->respond($user);
     }
 
-    public function generateJwt($user) {
-        $secret_key = "YOUR_SECRET_KEY";
+    // update user
+    function update($id)
+    {
+        try {
+            $user = $this->createObjectFromPostedJson("Models\\User");
+            $user = $this->service->update($user, $id);
+        } catch (Exception $e) {
+            $this->respondWithError(500, $e->getMessage());
+        }
+        $this->respond($user);
+    }
 
-        $issuer = "THE_ISSUER"; // this can be the domain/servername that issues the token
-        $audience = "THE_AUDIENCE"; // this can be the domain/servername that checks the token
-
-        $issuedAt = time(); // issued at
-        $notbefore = $issuedAt; //not valid before 
-        $expire = $issuedAt + 600; // expiration time is set at +600 seconds (10 minutes)
-
-        // JWT expiration times should be kept short (10-30 minutes)
-        // A refresh token system should be implemented if we want clients to stay logged in for longer periods
-
-        // note how these claims are 3 characters long to keep the JWT as small as possible
-        $payload = array(
-            "iss" => $issuer,
-            "aud" => $audience,
-            "iat" => $issuedAt,
-            "nbf" => $notbefore,
-            "exp" => $expire,
-            "data" => array(
-                "id" => $user->id,
-                "username" => $user->username,
-                "email" => $user->email
-        ));
-
-        $jwt = JWT::encode($payload, $secret_key, 'HS256');
-
-        return 
-            array(
-                "message" => "Successful login.",
-                "jwt" => $jwt,
-                "username" => $user->username,
-                "expireAt" => $expire
-            );
-    }    
+    // delete user
+    function delete($id)
+    {
+        try {
+            $user = $this->service->delete($id);
+        } catch (Exception $e) {
+            $this->respondWithError(500, $e->getMessage());
+        }
+        $this->respond("User deleted");
+    }
 }
